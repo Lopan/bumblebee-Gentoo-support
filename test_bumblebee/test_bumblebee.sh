903e8142397181e610632ecc3c61a6be4cccc46e
Iegor.Danylchenko <rmtdev@gmail.com>
2011-06-03 18:04:51 +0300

    corrected some issues with test scripts and re-digested.

--- test_bumblebee/test_bumblebee.sh
+++ test_bumblebee/test_bumblebee.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Testing bumblebee on my machine
 
-xinit /home/rainman/src/bumblebee-gentoo-support/app-misc/bumblebee/init_test.xinitrc -- :1 -config /etc/X11/xorg.conf.nvidia
+xinit /var/lib/layman/bumblebee/test_bumblebee/init_test.xinitrc -- :1 -config /etc/X11/xorg.conf.nvidia
