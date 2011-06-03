2b15c2de6b33aeb9dd92ea32f957c31471ef6fe8
Iegor.Danylchenko <rmtdev@gmail.com>
2011-06-03 20:31:32 +0300

    modified some info in xdm-optimus

--- test_bumblebee/test_bumblebee.sh
+++ test_bumblebee/test_bumblebee.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Testing bumblebee on my machine
 
-xinit /var/lib/layman/bumblebee/test_bumblebee/init_test.xinitrc -- :1 -config /etc/X11/xorg.conf.nvidia
+#xinit /var/lib/layman/bumblebee/test_bumblebee/init_test.xinitrc -- :1 -config /etc/X11/xorg.conf.nvidia
+xinit /var/lib/layman/bumblebee/test_bumblebee/init_test.xinitrc -- :1
